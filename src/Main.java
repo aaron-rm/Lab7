e9ad69ab0fbd39da67a8a951bddfafe14b6b09fc
Aaron <aaron.remarchuk@utp.ac.pa>
2025-06-18 14:50:32 -0500

    problema 1 listo

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,66 @@ import problema2.*;
 import problema3.*;
 import problema4.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Main {
     public static void main(String[] args) {
+        int opcion=0;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true){
+            try {
+                System.out.printf("\n\tMenú\n");
+                System.out.println("1- Problema 1");
+                System.out.println("2- Problema 2");
+                System.out.println("3- Problema 3");
+                System.out.println("4- Problema 4");
+                System.out.println("Cualquier otro número para salir");
+                System.out.print("Ingrese una opción: ");
+                opcion = Integer.parseInt(reader.readLine());
 
+                switch (opcion){
+                    case 1:{
+                        System.out.print("Ingrese número de filas: ");
+                        int n = Integer.parseInt(reader.readLine());
+                        System.out.print("Ingrese número de columnas: ");
+                        int m = Integer.parseInt(reader.readLine());
+                        Problema1 matriz = new Problema1(n,m);
+
+                        matriz.datosMatriz(reader);
+                        matriz.imprimirMatriz();
+
+                        break;
+                    }
+                    case 2:{
+                        break;
+                    }
+                    case 3:{
+                        break;
+                    }
+                    case 4:{
+                        break;
+                    }
+                    default:{
+                        break;
+                    }
+                }
+                break;
+            }
+            catch (NumberFormatException e){
+                System.err.println("Error, ingrese un digito: "+ e.getMessage());
+                continue;
+            }
+            catch (RuntimeException e){
+                System.err.println("Error: "+ e.getMessage());
+                continue;
+            }
+            catch (IOException e){
+                System.err.println("Error: "+ e.getMessage());
+                continue;
+            }
+        }
     }
 }
--- src/problema1/Problema1.java
+++ src/problema1/Problema1.java
@@ -2,5 +2,57 @@ package problema1;
 //Diseñe un programa que permita al usuario ingresar una matriz entera de n x m. Haga una
 //segunda matriz con la traspuesta de la matriz original. Imprima ambas matrices.
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Problema1 {
+    private int[][] matriz;
+    private int[][] matrizTraspuesta;
+    private int filas,columnas;
+
+    public Problema1(int n, int m){
+        this.filas=n;
+        this.columnas=m;
+        this.matriz = new int[n][m];
+        this.matrizTraspuesta = new int[m][n];
+    }
+
+    public void datosMatriz(BufferedReader reader){
+        try {
+            for (int i=0;i<filas;i++){
+                for (int j=0;j<columnas;j++){
+                    System.out.printf("\nIngrese el número en la posicion [%d][%d]: ",i,j);
+                    matriz[i][j] = Integer.parseInt(reader.readLine());
+                    matrizTraspuesta[j][i]=matriz[i][j];
+                }
+            }
+        }
+        catch (NumberFormatException e){
+            System.err.println("Error: ingrese un dígito " + e.getMessage());
+        }
+        catch (RuntimeException e){
+            System.err.println("Error: " + e.getMessage());
+        }
+        catch (IOException e){
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    public void imprimirMatriz(){
+        System.out.println("Matriz original");
+        for (int i=0;i<filas;i++){
+            for (int j=0;j<columnas;j++){
+                System.out.printf("%d\t", matriz[i][j]);;
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Matriz Traspuesta");
+        for (int i=0;i<columnas;i++){
+            for (int j=0;j<filas;j++){
+                System.out.printf("%d\t", matrizTraspuesta[i][j]);;
+            }
+            System.out.println();
+        }
+    }
 }
