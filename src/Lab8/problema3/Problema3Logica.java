8f4455c92cd4f55a287416c2fd7015c4c88ea1fd
KevinKaki14 <kevin.kakiyama14@gmail.com>
2025-07-11 19:08:52 -0500

    Update Problema3Logica.java

--- src/Lab8/problema3/Problema3Logica.java
+++ src/Lab8/problema3/Problema3Logica.java
@@ -1,29 +1,52 @@
 package Lab8.problema3;
 
-// Un banco ofrece a sus clientes diversos tipos de depósitos a plazo fijo. Realice un
-//programa que permita calcular el monto acumulado de un plazo fijo.
-//Tome los datos del cliente (Nombre, Cédula) y cantidad que desea depositar. El
-//mínimo para abrir la cuenta es de B/2000.00
-//Con la siguiente tabla se determina la tasa de interés anual. Utilice la fórmula de
-//interés compuesto: A=P×(1+r)n , donde:
-//A: monto acumulado
-//P: deposito inicial
-//r = tasa de interés por período
-//n= número de períodos(en años)
-//12 meses
-//4%
-//24 meses
-//4.5%
-//36 meses
-//4.55%
-//48 meses
-//4.75 %
-//60 meses
-//5%
-//Imprima cuanto obtendrá el cliente al final del plazo establecido.
-
-
-
 public class Problema3Logica {
+  private String nombre;
+    private int cedula;
+    private double deposito;
+    private double tasa;
+    private int meses;
+    private double intAnual;
 
+    public Problema3Logica(String nombre, int cedula, double deposito) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.deposito = deposito;
+    }
+
+    public double getIntAnual() {
+        return intAnual;
+    }
+
+    public void definirTasaAnual(int opcion) {
+        switch (opcion) {
+            case 1:
+                meses = 12;
+                tasa = 0.04;
+                break;
+            case 2:
+                meses = 24;
+                tasa = 0.045;
+                break;
+            case 3:
+                meses = 36;
+                tasa = 0.0455;
+                break;
+            case 4:
+                meses = 48;
+                tasa = 0.0475;
+                break;
+            case 5:
+                meses = 60;
+                tasa = 0.05;
+                break;
+            default:
+                System.out.println("Opción inválida.");
+        }
+    }
+
+    public void calcularInteresAnual() {
+        double n = meses / 12.0;
+        intAnual = deposito * Math.pow(1 + tasa, n);
+    }
 }
